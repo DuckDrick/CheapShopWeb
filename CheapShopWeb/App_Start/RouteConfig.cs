2e098e04e3a8b0e021f3bec1da8bab33b7b479fe
JuozapasG <juozapas.gelcys@gmail.com>
2020-11-10 22:41:59 +0300

    search page show novastar products

--- CheapShopWeb/App_Start/RouteConfig.cs
+++ CheapShopWeb/App_Start/RouteConfig.cs
@@ -18,6 +18,11 @@ namespace CheapShopWeb
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+            routes.MapRoute(
+                name: "Search",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Search", action = "Search", id = UrlParameter.Optional }
+            );
         }
     }
 }
---
+++ CheapShopWeb/Controllers/SearchController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CheapShopWeb.DataContext;
+
+namespace CheapShopWeb.Controllers
+{
+    public class SearchController : Controller
+    {
+        // GET: Search
+        private ProductDbContext _db;
+
+        public SearchController()
+        {
+            _db = new ProductDbContext();
+        }
+
+        public ActionResult Search()
+        {
+            var products = _db.Products.ToArray();
+            return View(products);
+        }
+    }
+}

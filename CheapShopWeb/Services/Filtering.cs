a7df5a6da68d10bbb0b4a9d5445b7d3bed12105f
Andrius Bertašius <andrius.bertasius176@gmail.com>
2020-11-11 22:19:27 +0200

    Changed controller to work with new table and autogenerated views

---
+++ CheapShopWeb/Services/Filtering.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CheapShopWeb.Models;
+
+namespace CheapShopWeb.Services
+{
+    public class Filtering
+    {
+        public static List<Product> Filter(string name, string min, string max, string group, string source)
+        {
+            if (name != null)
+            {
+
+            }
+            if (min != null)
+            {
+
+            }
+            if (max != null)
+            {
+
+            }
+            if (group != null)
+            {
+
+            }
+            if (source != null)
+            {
+
+            }
+        }
+    }
+}

55ff44a5227d88b187a3f2db810a1f8a13c817af
Andrius Bertašius <andrius.bertasius176@gmail.com>
2020-11-11 22:48:05 +0200

    minor change

--- CheapShopWeb/Services/Filtering.cs
+++ CheapShopWeb/Services/Filtering.cs
@@ -10,29 +10,29 @@ namespace CheapShopWeb.Services
     {
         public static List<Product> Filter(List<Product> productList, string name, string min, string max, string groups, string sources)
         {
-            var filteredList = new List<Product>(productList);
+
             if (name != null)
             {
-                filteredList = filteredList.FindAll(product => product.name.ToLower().Contains(name.ToLower()));
+                productList = productList.FindAll(product => product.name.ToLower().Contains(name.ToLower()));
             }
             if (min != null)
             {
-                filteredList = filteredList.FindAll(product => float.Parse(product.price.Replace('.',',')) >= float.Parse(min));
+                productList = productList.FindAll(product => float.Parse(product.price.Replace('.',',')) >= float.Parse(min));
             }
             if (max != null)
             {
-                filteredList = filteredList.FindAll(product => float.Parse(product.price.Replace('.', ',')) <= float.Parse(max));
+                productList = productList.FindAll(product => float.Parse(product.price.Replace('.', ',')) <= float.Parse(max));
             }
             if (groups != null)
             {
-                filteredList = filteredList.FindAll(product => groups.Split(' ').Any(group => product.group.Equals(group)));
+                productList = productList.FindAll(product => groups.Split(' ').Any(group => product.group.Equals(group)));
             }
             if (sources != null)
             {
-                filteredList = filteredList.FindAll(product => sources.Split(' ').Any(source => product.source.Equals(source)));
+                productList = productList.FindAll(product => sources.Split(' ').Any(source => product.source.Equals(source)));
             }
 
-            return filteredList;
+            return productList;
         }
     }
 }

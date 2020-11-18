d290589911bbbc69f5c2e2a947f63a8c5e8f47aa
Andrius Bertašius <andrius.bertasius176@gmail.com>
2020-11-19 00:55:50 +0200

    added paged to search page

--- CheapShopWeb/App_Start/BundleConfig.cs
+++ CheapShopWeb/App_Start/BundleConfig.cs
@@ -24,7 +24,8 @@ namespace CheapShopWeb
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/PagedList.css"));
         }
     }
 }

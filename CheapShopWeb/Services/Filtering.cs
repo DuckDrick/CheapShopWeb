fc67108ed74659ecc0e98335c087b9c3f44074cf
JuozapasG <juozapas.gelcys@gmail.com>
2020-11-22 22:58:58 +0300

    tried to find algorithm to find most similar products, kinda works

--- CheapShopWeb/Services/Filtering.cs
+++ CheapShopWeb/Services/Filtering.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Web;
@@ -61,7 +62,10 @@ namespace CheapShopWeb.Services
             newlist.Add(prod);
             foreach (var product in productList)
             {
-                newlist.Add(product);
+                if (LevenshteinDistance.Compute(product.name, prod.name) <= 4*prod.name.Split(' ').Length) //or 3*//
+                {
+                    newlist.Add(product);
+                }
             }
             return newlist;
         }
@@ -71,5 +75,67 @@ namespace CheapShopWeb.Services
             float n = float.Parse(num) * 100;
             return n;
         };
+        private class NearMatchComparer : IEqualityComparer<string>
+        {
+            public bool Equals(string x, string y)
+            {
+                return string.Compare(x, y) < 2;
+            }
+
+            public int GetHashCode(string obj)
+            {
+                return obj.GetHashCode();
+            }
+        }
+    }
+    static class LevenshteinDistance
+    {
+        /// <summary>
+        /// Compute the distance between two strings.
+        /// </summary>
+        public static int Compute(string s, string t)
+        {
+            int n = s.Length;
+            int m = t.Length;
+            int[,] d = new int[n + 1, m + 1];
+
+            // Step 1
+            if (n == 0)
+            {
+                return m;
+            }
+
+            if (m == 0)
+            {
+                return n;
+            }
+
+            // Step 2
+            for (int i = 0; i <= n; d[i, 0] = i++)
+            {
+            }
+
+            for (int j = 0; j <= m; d[0, j] = j++)
+            {
+            }
+
+            // Step 3
+            for (int i = 1; i <= n; i++)
+            {
+                //Step 4
+                for (int j = 1; j <= m; j++)
+                {
+                    // Step 5
+                    int cost = (t[j - 1] == s[i - 1]) ? 0 : 1;
+
+                    // Step 6
+                    d[i, j] = Math.Min(
+                        Math.Min(d[i - 1, j] + 1, d[i, j - 1] + 1),
+                        d[i - 1, j - 1] + cost);
+                }
+            }
+            // Step 7
+            return d[n, m];
+        }
     }
 }
